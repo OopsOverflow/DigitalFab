60a8b918743c140575f1b37390aa09d7a84fbdf7
OopsOverflow <houssemmahmoud18@gmail.com>
2023-01-24 15:49:11 +0100

    Init project and configs using Nextra

---
+++ theme.config.jsx
@@ -0,0 +1,9 @@
+export default {
+  logo: <span>Digital Fab Journey</span>,
+  project: {
+    link: 'https://github.com/OopsOverflow/digitalfab',
+  },
+  sidebar: {
+    toggleButton: true,
+  },
+};
